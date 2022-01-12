d68d5b45d5265fa1f6a92e97a9d20585db73e599
AmanSharma <usernameofaman@gmail.com>
2022-01-13 00:10:54 +0530

    added DB

--- config/env/development.js
+++ config/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB: process.env.DB || '',
+  DB: process.env.DB || 'mongodb+srv://macbook:macbook@cluster0.ztfod.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   PORT: process.env.PORT || '3000',
   IS_CONSOLE_LOG: process.env.IS_CONSOLE_LOG || 'true'
 }
